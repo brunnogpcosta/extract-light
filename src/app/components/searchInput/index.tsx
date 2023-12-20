279212b3c8c8e587c9fc8fb07e62e35e45435065
Brunno Costa <b2ncostadev@gmail.com>
2023-12-20 19:29:06 -0300

    framer lib included on faturas page

--- src/app/components/searchInput/index.tsx
+++ src/app/components/searchInput/index.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+"use client";
 
+import React, { useState } from "react";
 
-const SearchInput = () =>{
-    return(
-    <div className="rounded-md w-full sm:w-auto">
-        <input type="text" placeholder="Nº do cliente" className="border-0 bg-[#3C3C43] p-2 rounded-md text-[#FFFFFF] w-full"/>
-    </div>)
+interface ISearchInput{
+    searchString: string
 }
 
+const SearchInput: React.FC<ISearchInput> = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchInput = (text: string) => {
+    setTimeout(() => {
+      setSearchText(text);
+    }, 1000);
+  };
+
+  return (
+    <div className="rounded-md w-full sm:w-auto">
+      <input
+        type="text"
+        onChange={(e) => handleSearchInput(e.target.value)}
+        placeholder="Nº do cliente"
+        className="border-0 bg-[#3C3C43] p-2 rounded-md text-[#FFFFFF] w-full"
+      />
+    </div>
+  );
+};
 
 export default SearchInput;
--- src/app/faturas/page.tsx
+++ src/app/faturas/page.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import { FaFileDownload } from "react-icons/fa";
+import { motion } from "framer-motion";
+
 import "./style.css";
 
 const invoices = [
@@ -43,26 +45,44 @@ const invoices = [
   },
 ];
 
-const Faturas = () => {
-  const [vibratingId, setVibratingId] = useState('');
+const list = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  }
+  
+  const item = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: -100 },
+  }
+  
 
+const Faturas = () => {
+  const [vibratingId, setVibratingId] = useState("");
 
   return (
-    <div className="w-full grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-8">
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={list}
+      className="w-full grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-8"
+    >
       {invoices.map((inv) => (
-        <div
+        <motion.div
+          variants={item}
           key={inv.id}
           className="cursor-pointer bg-[#1E1E22] min-w-[200px] h-[200px] sm:mx-8 mb-8 sm:mb-0 rounded-md p-4 flex flex-col items-center justify-center"
           onMouseEnter={() => setVibratingId(inv.id)}
         >
           <FaFileDownload
             size={40}
-            className={`text-[#15D47B] mb-4 ${vibratingId === inv.id ? "vibrate" : ""}`}
+            className={`text-[#15D47B] mb-4 ${
+              vibratingId === inv.id ? "vibrate" : ""
+            }`}
           />
           <p>{inv.period}</p>
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
